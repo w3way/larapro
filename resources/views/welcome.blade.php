d965d94c255dc81ca8d909d506890f6b680f9cd1
w3way <w3way07861@gmail.com>
2022-02-17 18:51:22 +0530

    Just added livewire styles and script directives

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
+        <title>Livewire</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        @livewireStyles
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -26,8 +25,10 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <h1>Hello World! Changed</h1>
+                <h1>Hello World!</h1>
             </div>
         </div>
+
+        @livewireScripts
     </body>
 </html>
